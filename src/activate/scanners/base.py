415e717855c54a37a4829b3fe6d889c8c93163a8
Samuel Spencer <theodore.therone@gmail.com>
2025-10-02 03:24:11 +0000

    changes

--- src/activate/scanners/base.py
+++ src/activate/scanners/base.py
@@ -6,6 +6,7 @@ from activate.utils.exceptions import ConfigError
 from activate.utils.loaddb import prepare_engine
 from activate.utils.progress import NullProgressReporter
 
+
 scanners_register = {
 }
 
@@ -13,11 +14,31 @@ def register(scanner_class):
     scanners_register[scanner_class.name] = scanner_class
 
 
+# We can't use truthiness, or None as both of these are valid values which we may want to retain and pass through.
+VALUE_NOT_SET = "This value has not been set"
+
+
 class Scanner:
+    namespace_separator = "::"
+    hashing_method = hashlib.sha256
+
     def __init__(self, config, progress_callback=NullProgressReporter()):
         self.config = config
         self.progress = progress_callback
         self._metadata = defaultdict(dict)
+
+        self.activate_options = self.config.config.get('activate_options', {})
+        ns_sep = self.activate_options.get('namespace_separator', None)
+        if ns_sep is not None:
+            # Check against none, as empty string is a valid entry
+            self.namespace_separator = ns_sep
+
+        if hash_method := self.activate_options.get('hashing_method', None):
+            if hash_method == 'md5':
+                self.hashing_method = hashlib.md5
+            if hash_method == 'plaintext':
+                self.hashing_method = lambda plaintext: plaintext
+        self.output_plaintext_with_hash = bool(self.activate_options.get('output_plaintext_with_hash', False))
 
     def scan_datasets(self):
         raise NotImplementedError
@@ -30,26 +51,28 @@ class Scanner:
         raise NotImplementedError
 
     def make_constant_ns_id(self, metadatatype: str, identifier: str) -> str:
-        message = hashlib.sha256(
+        message = self.hashing_method(
             f"ARISTOTLE_ACTIVATE_NAMESPACE::{metadatatype}::{identifier}".encode("utf-8")
         ).hexdigest()
         return f"active_id:v1:{metadatatype}:{message}"
 
     def make_active_id(self, metadatatype, identifier: str) -> str:
-        message = hashlib.sha256(
-            f"{self.active_id_namespace}::{identifier}".encode("utf-8")
-        ).hexdigest()
-        return f"active_id:v1:{metadatatype}:{message}"
+        plaintext_id = f"{self.active_id_namespace}{self.namespace_separator}{identifier}".encode("utf-8")
+        message = self.hashing_method(plaintext_id).hexdigest()
+        active_id = f"active_id:v1:{metadatatype}:{message}"
+        if self.output_plaintext_with_hash:
+            active_id = f"{active_id}#{plaintext_id}"
+        return active_id
 
     def make_active_column_id(self, identifier: str, column_name: str) -> str:
-        message = hashlib.sha256(
-            f"{self.active_id_namespace}::{identifier}:{column_name}".encode("utf-8")
+        message = self.hashing_method(
+            f"{self.active_id_namespace}{self.namespace_separator}{identifier}:{column_name}".encode("utf-8")
         ).hexdigest()
         return f"active_id:v1:dist_path:{message}"
 
     def make_active_table_link_id(self, identifier: str) -> str:
-        message = hashlib.sha256(
-            f"{self.active_id_namespace}:{identifier}".encode("utf-8")
+        message = self.hashing_method(
+            f"{self.active_id_namespace}{self.namespace_separator}{identifier}".encode("utf-8")
         ).hexdigest()
         return f"active_id:v1:dataset_distribution:{message}"
 
@@ -82,3 +105,104 @@ class Scanner:
         #     "data_element": {},
         # }
         self._metadata[item_type][active_id] = item
+
+    def upsert_metadata(self, item_type, active_id, item={}):
+        if active_id not in self._metadata[item_type].keys():
+            self._metadata[item_type][active_id] = item
+        
+    def append_metadata_field(self, item_type, active_id, field, value):
+        self.upsert_metadata(item_type, active_id)
+        self._metadata[item_type][active_id].setdefault(field, [])
+        self._metadata[item_type][active_id][field].append(value)
+
+    def scan_metadata(self):
+        self.scan_datasets()
+
+    def field_to_aristotle(self, conf, source):
+        """
+        Maps and converts a single field to Aristotle's format.
+        source is a dictionary of keys/values
+        conf is an aristotle field configuration
+
+        1. If conf is a string, it is the name of the field to extract
+        2. If conf is a list, it is a list of field configurations to extract and concatenate
+        3. If conf is a dict, it is a configuration for a specific operation
+            a. type: concat - concatenate a list of field configurations
+            b. type: field - extract a single field
+            c. type: parse_date - extract a single field and parse it as a date
+            d. type: string - return a constant from the configuration cast as a string
+            e. type: literal - return a constant from the configuration without any data type modification
+            f. type: geojson_to_bbox - extract a geojson field and convert it to a bbox
+            g. type: join - extract a list field and join it with a separator
+            h. type: list - extract a list of field configurations and return as a list
+            i. type: dict - extract a dict of field configurations and return as a dict
+        """
+
+        final_value = VALUE_NOT_SET
+        default_value = VALUE_NOT_SET
+        if type(conf) is str:
+            conf = {
+                "type": "field",
+                "field": conf
+            }
+            default_value = "Unknown value"
+
+        if type(conf) is list:
+            pass
+        elif type(conf) is dict:
+            if conf['type'] == 'concat':
+                values = []
+                for inner_conf in conf['values']:
+                    values.append(self.field_to_aristotle(inner_conf, source))
+                separator = conf.get('separator', '')
+                final_value = separator.join(values)
+            elif conf['type'] == 'field':
+                final_value = source.get(conf['field'], None)
+            elif conf['type'] == 'parse_date':
+                value = source.get(conf['field'], None)
+                if value:
+                    try:
+                        format_string = conf.get('format', '%Y-%m-%dT%H:%M:%S%z')
+                        dt = datetime.strptime(value, format_string)
+                        final_value = dt.isoformat()
+                    except:
+                        final_value = OMIT_FIELD
+                else:
+                    final_value = OMIT_FIELD
+            elif conf['type'] == 'string':
+                final_value = str(conf['value'])
+            elif conf['type'] == 'literal':
+                final_value = conf['value']
+            elif conf['type'] == 'geojson_to_bbox':
+                if geojson_obj := source.get(conf['field'], None):
+                    tp = self.geojson_to_turfpy(geojson_obj)
+                    bbox_coords = bbox(tp)
+                    final_value = str(bbox_coords)
+                else:
+                    final_value = OMIT_FIELD
+            elif conf['type'] == 'join':
+                values = source.get(conf['field'], [])
+                separator = conf.get('separator', '')
+                if values:
+                    final_value = separator.join(values)
+            elif conf['type'] == 'list':
+                values = []
+                for inner_conf in conf['values']:
+                    values.append(self.field_to_aristotle(inner_conf, source))
+                final_value = values
+            elif conf['type'] == 'dict':
+                value = {}
+                for key, item in conf['keys'].items():
+                    output = self.field_to_aristotle(item, source)
+                    if output != OMIT_FIELD:
+                        value[key] = output
+                final_value = value
+
+            if default_value == VALUE_NOT_SET:
+                default_value = conf.get("default", "Unknown value")
+            if conf.get("on_null", "keep") == "set_default":
+                final_value = VALUE_NOT_SET     
+
+        if final_value == VALUE_NOT_SET:
+            final_value = default_value
+        return final_value
